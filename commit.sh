12be873dfa06fca82d611f9c83b2bcbb91b1c263
Eddie Wynman <edward.wynman@gmail.com>
2023-05-15 16:27:54 -0400

    update commit script

--- commit.sh
+++ commit.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-git add .
-git commit -m "Automated commit"
-git push
+# Get the commit message from the user
+echo "Enter commit message:"
+read message
 
+# Run build
+npm run build
+
+# Add all files to staging area
+git add .
+
+# Commit changes with the specified message
+git commit -m "$message"
+
+# Push changes to the remote repository
+git push origin master
+
+echo "Changes pushed to remote repository."
